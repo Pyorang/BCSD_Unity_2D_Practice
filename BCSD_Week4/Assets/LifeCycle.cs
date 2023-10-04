c5f296cbfdcf23d894989c38ae57515a1ba0d268
Pyorang <sudangi143@gmail.com>
2023-10-05 00:28:06 +0900

    Lesson7

--- BCSD_Week4/Assets/LifeCycle.cs
+++ BCSD_Week4/Assets/LifeCycle.cs
@@ -4,45 +4,72 @@ using UnityEngine;
 
 public class LifeCycle : MonoBehaviour
 {
-    void Awake()
+    /*
+    void Update()
     {
-        Debug.Log("플레이어 데이터가 준비되었습니다.");
-    }
+        if (Input.anyKeyDown)
+            Debug.Log("플레이어가 아무 키를 눌렀습니다.");
 
-    void OnEnable()
-    {
-        Debug.Log("플레이어가 로그인하였습니다.");
-    }
+        
+        if (Input.anyKey)
+            Debug.Log("플레이어가 아무 키를 누르고 있습니다.");
+        
+        if (Input.GetKeyDown(KeyCode.Return))
+            Debug.Log("아이템을 구입하였습니다.");
 
+        if (Input.GetKey(KeyCode.LeftArrow))
+            Debug.Log("왼쪽으로 이동 중");
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+            Debug.Log("오른쪽으로 이동을 멈추었습니다.");
+
+        if (Input.GetMouseButtonDown(0))
+            Debug.Log("미사일 발사!");
+
+        if (Input.GetMouseButton(0))
+            Debug.Log("미사일 모으는 중...");
+
+        if (Input.GetMouseButtonUp(0))
+            Debug.Log("슈퍼 미사일 발사!!");
+
+        if (Input.GetButtonDown("Fire1"))
+            Debug.Log("점프!");
+
+        if (Input.GetButton("Fire1"))
+            Debug.Log("점프 모으는 중...");
+
+        if (Input.GetButtonUp("Fire1"))
+            Debug.Log("슈퍼 점프!!");
+
+        if (Input.GetButton("Horizontal"))
+        {
+            Debug.Log("횡 이동 중..."+Input.GetAxis("Horizontal"));
+        }
+
+        if (Input.GetButton("Horizontal"))
+        {
+            Debug.Log("횡 이동 중..." + Input.GetAxisRaw("Horizontal"));
+        }
+
+        if (Input.GetButton("Vertical"))
+        {
+            Debug.Log("종 이동 중..." + Input.GetAxisRaw("Vertical"));
+        }
+    }
+    */
+
+    /*
     void Start()
     {
-        Debug.Log("사냥 장비를 챙겼습니다.");
+        Vector3 vec = new Vector3(5, 0, 0);
+        transform.Translate(vec);
     }
-
-    void FixedUpdate()
-    {
-        Debug.Log("이동~");
-    }
+    */
 
     void Update()
     {
-        Debug.Log("몬스터 사냥!!");
+        //Vector3 vec = new Vector3(0, 0.1f, 0);
+        Vector3 vec = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.Translate(vec);
     }
-
-    void LateUpdate()
-    {
-        Debug.Log("경험치 획득.");
-    }
-
-    void OnDisable()
-    {
-        Debug.Log("플레이어가 로그아웃 하였습니다.");
-    }
-
-    void OnDestroy()
-    {
-        Debug.Log("플레이어 데이터를 해제하였습니다.");
-    }
-
-
 }
